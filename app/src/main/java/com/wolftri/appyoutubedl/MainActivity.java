e3ff63f28e8b8fdde66e5ef9a9e1fadd852682e9
dogiloki <wolftri2002@gmail.com>
2023-08-20 05:50:32 -0600

    Solución de error al transferir archivos es dispositivos móviles físicos

--- app/src/main/java/com/wolftri/appyoutubedl/MainActivity.java
+++ app/src/main/java/com/wolftri/appyoutubedl/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.documentfile.provider.DocumentFile;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -17,37 +21,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import multitaks.code.Code;
 import multitaks.directory.FileBlock;
 import multitaks.directory.Storage;
+import multitaks.directory.enums.DirectoryType;
 import multitaks.socket.SocketClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_CODE_CAMARE=0;
 
     private SocketClient socket=new SocketClient();
     private Handler uiHandler=new Handler(Looper.getMainLooper());
     private Storage s;
     private FileBlock file;
-    private String path=Environment.getExternalStorageDirectory().getAbsolutePath();
+    private String path=Environment.getExternalStorageDirectory().getAbsolutePath()+"/yt-dlp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.initComponents();
+        //this.path=getExternalFilesDir("descargas").getAbsolutePath();
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,new String[]{
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     Manifest.permission.READ_EXTERNAL_STORAGE
             },1);
         }
-        Storage.createFolder(path+"/yt-dlp");
+        Storage.exists(this.path,DirectoryType.FOLDER,true);
+    }
+
+    public void test(View v){
+        text_info.setText(this.path);
     }
 
     public void startSocket(String ip, int port){
@@ -67,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                         try {
-                            s = new Storage(path+"/yt-dlp/"+name);
-                            file = s.fileBlock(8192);
+                            s = new Storage(path+"/"+name);
+                            file = s.fileBlock(1048576);
                             socket.emit("file_name_success", "true");
                         } catch (IOException ex) {
                             uiHandler.post(new Runnable() {
@@ -119,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         IntentIntegrator integrator=new IntentIntegrator(MainActivity.this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
         integrator.setPrompt("Escanea el código QR");
-        integrator.setCameraId(0);
+        integrator.setCameraId(REQUEST_CODE_CAMARE);
         integrator.setBeepEnabled(true);
         integrator.setBarcodeImageEnabled(true);
         integrator.initiateScan();
@@ -129,16 +148,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int req_code, int res_code, @Nullable Intent data) {
         super.onActivityResult(req_code,res_code,data);
         IntentResult result=IntentIntegrator.parseActivityResult(req_code,res_code,data);
-        if(result==null){
-            text_info.setText("Error al escanear");
-            return;
+        switch(req_code){
+            case REQUEST_CODE_CAMARE:{
+                if(result==null){
+                    text_info.setText("Error al escanear");
+                    return;
+                }
+                if(result.getContents()==null){
+                    text_info.setText("Se cancelo el escanear");
+                    return;
+                }
+                String[] chain=result.getContents().split(":");
+                this.startSocket(chain[0],Integer.parseInt(chain[1]));
+                break;
+            }
         }
-        if(result.getContents()==null){
-            text_info.setText("Se cancelo el escanear");
-            return;
-        }
-        String[] chain=result.getContents().split(":");
-        this.startSocket(chain[0],Integer.parseInt(chain[1]));
     }
 
     private void initComponents(){
